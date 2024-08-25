d64e355d361e0730798b8bbd9ef409771bd25dc8
dikshagupta3094 <dikshagpragya@gmail.com>
2024-08-25 19:43:27 +0530

    Add director image

--- src/Layout/HomeLayout.jsx
+++ src/Layout/HomeLayout.jsx
@@ -2,29 +2,97 @@ import React from "react";
 import sathm from "../assets/sathm_logo_without_background.png";
 import Devi_Ahilya_Vishwavidyalaya_Logo from "../assets/Devi_Ahilya_Vishwavidyalaya_Logo.png";
 import { NavLink } from "react-router-dom";
-import { RxHamburgerMenu } from "react-icons/rx";
-import image_bg from '../assets/image_bg.jpg'
+import { BsAirplaneFill } from "react-icons/bs";
+import { FaHome } from "react-icons/fa";
+import { SiBookstack } from "react-icons/si";
+import { FaPeopleGroup } from "react-icons/fa6";
+import { FaHandshake } from "react-icons/fa";
+import { RxCross2 } from "react-icons/rx";
+import { FaIndustry } from "react-icons/fa";
+import image_bg from "../assets/image_bg.jpg";
+import Footers from "../Components/Footers";
+
+//drawer
+import { styled, useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import CssBaseline from "@mui/material/CssBaseline";
+import MuiAppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+
+const drawerWidth = 250;
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  transition: theme.transitions.create(["margin", "width"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: `${drawerWidth}px`,
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: "flex-end",
+}));
 
 function HomeLayout({ children }) {
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
   return (
     <div>
       {/* Logo design and text design */}
-      <div className="flex flex-col items-center justify-center p-3 md:bg-gray-100 bg-cover "  style={{ backgroundImage: `url(${image_bg})` }}>
+      <div
+        className="flex flex-col items-center justify-center p-3 md:bg-gray-100 bg-cover "
+        style={{ backgroundImage: `url(${image_bg})` }}
+      >
         <div className="flex flex-col md:flex-row items-center justify-between w-full space-y-4 md:space-y-0  md:space-x-4 lg:space-x-8">
-          {/* on medium size design */} 
+          {/* on medium size design */}
           <img
             src={Devi_Ahilya_Vishwavidyalaya_Logo}
-            className="md:w-32 md:h-32 md:block hidden"
+            className="md:w-36 md:h-36 md:block hidden"
             alt="Devi Ahilya Logo"
           />
           {/* For smaller device */}
           <img
             src={sathm}
-            className="block md:hidden w-40 h-40"
+            className="block md:hidden w-40 h-40 sm:"
             alt="Small Device Logo"
           />
           <div className="text-center md:px-2 lg:px-2">
-            <h1 className="md:text-3xl lg:text-4xl text-lg font-bold  text-blue-900">
+            <h1 className="md:text-3xl lg:text-4xl text-lg font-bold  text-blue-900 sm:px-0 py-0">
               SCHOOL OF AVIATION, TOURISM AND HOSPITALITY MANAGEMENT
             </h1>
             <span className="text-lg md:text-xl lg:text-2xl text-blue-900  font-bold">
@@ -33,37 +101,173 @@ function HomeLayout({ children }) {
             <p className="text-lg md:text-2xl lg:text-3xl  md:text-gray-800 text-blue-900  mt-2 tracking-wider font-bold text-center">
               DEVI AHILYA VISHWAVIDYALAYA, INDORE
             </p>
+            <p className="text-xl   md:text-gray-600 text-blue-900  mt-1 tracking-wider font-semibold text-center">
+              Accredited A+ Grade by NACC
+            </p>
           </div>
           <img
             src={sathm}
             alt="SATHM Logo"
-            className="md:w-36 md:h-36 md:block hidden"
+            className="md:w-40 md:h-40 md:block hidden"
           />
         </div>
       </div>
 
       {/* Navbar */}
-
       <div className="border  border-gray-400 "></div>
 
-      <div className="hidden md:flex items-center justify-evenly  bg-sky-600 py-4  text-white font-semibold text-xl">
-        <NavLink to="/" className="hover:underline">Home</NavLink>
-        <NavLink to="/about" className="hover:underline">About</NavLink>
-        <NavLink to="/academics" className="hover:underline">Academics</NavLink>
-        <NavLink to="/faculties" className="hover:underline">Faculties</NavLink>
-        <NavLink to="/events" className="hover:underline">Events & Activities</NavLink>
-        <NavLink to="/corporate_linkage" className="hover:underline">Corporate Linkages</NavLink>
-        <NavLink to="/mou" className="hover:underline">MOUs</NavLink>
+      <div className="hidden md:flex items-center justify-evenly  bg-sky-600 py-4  text-white font-semibold lg:text-xl sm:sm:text-[15px] w-full sticky top-0">
+        <NavLink to="/" className="hover:underline">
+          Home
+        </NavLink>
+        <NavLink to="/about" className="hover:underline">
+          About
+        </NavLink>
+        <NavLink to="/academics" className="hover:underline">
+          Academics
+        </NavLink>
+        <NavLink to="/faculties" className="hover:underline">
+          Faculties
+        </NavLink>
+        <NavLink to="/events" className="hover:underline">
+          Events & Activities
+        </NavLink>
+        <NavLink to="/corporate_linkage" className="hover:underline">
+          Corporate Linkages
+        </NavLink>
+        <NavLink to="/mou" className="hover:underline">
+          MOUs
+        </NavLink>
       </div>
 
-      <button className="block md:hidden bg-gray-100 p-2 m-2">
-        <RxHamburgerMenu className="w-7 h-7 text-blue-500" />
-      </button>
-      {/* Responsive menu */}
-      {/* <div className="fixed inset-0 bg-white">
+      {/* Responsive navbar */}
+      <Box className="flex md:hidden">
+        <CssBaseline />
+        <AppBar
+          position="fixed"
+          open={open}
+          style={{
+            backgroundColor: "transparent",
+            boxShadow: "none",
+            opacity: "initial",
+          }}
+        >
+          <Toolbar className="flex justify-between">
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              onClick={handleDrawerOpen}
+              edge="start"
+              sx={{ mr: 2, ...(open && { display: "none" }) }}
+            >
+              <MenuIcon />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+        <Drawer
+          sx={{
+            width: drawerWidth,
+            flexShrink: 0,
+            "& .MuiDrawer-paper": {
+              width: drawerWidth,
+              boxSizing: "border-box",
+            },
+          }}
+          variant="persistent"
+          anchor="left"
+          open={open}
+        >
+          <DrawerHeader>
+            <IconButton onClick={handleDrawerClose}>
+              {theme.direction === "ltr" ? <RxCross2 /> : <RxCross2 />}
+            </IconButton>
+          </DrawerHeader>
+          <Divider />
+          <List className="flex flex-col gap-y-2">
+            <NavLink to="/" onClick={handleDrawerClose}>
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <FaHome />
+                  </ListItemIcon>
+                  <ListItemText primary={"Home"}></ListItemText>
+                </ListItemButton>
+              </ListItem>
+            </NavLink>
 
-      </div> */}
+            <NavLink to="/about" onClick={handleDrawerClose}>
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <BsAirplaneFill />
+                  </ListItemIcon>
+                  <ListItemText primary={"About"}></ListItemText>
+                </ListItemButton>
+              </ListItem>
+            </NavLink>
+
+            <NavLink to="/academics" onClick={handleDrawerClose}>
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <SiBookstack />
+                  </ListItemIcon>
+                  <ListItemText primary={"Academics"}></ListItemText>
+                </ListItemButton>
+              </ListItem>
+            </NavLink>
+
+            <NavLink to="/faculties" onClick={handleDrawerClose}>
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <FaPeopleGroup />
+                  </ListItemIcon>
+                  <ListItemText primary={"Faculties"}></ListItemText>
+                </ListItemButton>
+              </ListItem>
+            </NavLink>
+
+            <NavLink to="/events" onClick={handleDrawerClose}>
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <CalendarMonthIcon />
+                  </ListItemIcon>
+                  <ListItemText primary={"Events & Activities"}></ListItemText>
+                </ListItemButton>
+              </ListItem>
+            </NavLink>
+
+            <NavLink to="/corporate_linkage" onClick={handleDrawerClose}>
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <FaIndustry />
+                  </ListItemIcon>
+                  <ListItemText primary={"Corporate Linkages"}></ListItemText>
+                </ListItemButton>
+              </ListItem>
+            </NavLink>
+
+            <NavLink to="/mou">
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <FaHandshake />
+                  </ListItemIcon>
+                  <ListItemText primary={"MOUs"}></ListItemText>
+                </ListItemButton>
+              </ListItem>
+            </NavLink>
+          </List>
+        </Drawer>
+      </Box>
+
       {children}
+
+      {/* Footer import */}
+      <Footers />
     </div>
   );
 }
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,96 @@
-import React from "react";
+import React, { useEffect } from "react";
 import HomeLayout from "../Layout/HomeLayout";
-
+import bgHome from "../assets/bgHome.jpg";
+import DirectorImg from "../assets/DirectorImg.jpg";
+import AOS from "aos";
+import "aos/dist/aos.css";
 function Home() {
+  useEffect(() => {
+    AOS.init({
+      offset: 300,
+      easing: "ease-in-sine",
+      duration: 500,
+    });
+  }, []);
   return (
     <HomeLayout>
-      <div>
-        
+      <div className="bg-cover" style={{ backgroundImage: `url(${bgHome})` }}>
+        <div className="flex flex-col items-center justify-center">
+          <p
+            className="max-w-[900px] text-justify p-3 text-gray-700 font-semibold mt-4 box-shadow mb-4 "
+            data-aos="fade-down"
+          >
+            Aviation management is a department responsible for coordinating
+            operations at an airport, airline or other businesses in the
+            aviation field. Professionals in this department specialise in
+            various aspects of the fields, such as flight logistics, aircraft
+            maintenance, customer service and marketing for the airline.
+            Individuals pursuing a career in this field need strong analytical
+            and leadership skills to become successful.
+          </p>
+
+          {/* Craousl */}
+           
+           {/* <div>
+             <h1>Aviation Industry Carrer</h1> 
+             <div className="flex gap-10 slide-tracker">
+              <img src="https://www.shutterstock.com/image-photo/cheerful-young-man-airline-worker-260nw-1815171710.jpg" alt="" className="hover:scale-110 duration-1000 w-[250px]"/>
+
+              <img src="https://www.shutterstock.com/image-photo/cheerful-young-man-airline-worker-260nw-1815171710.jpg" alt="" className="hover:scale-110 duration-1000 w-[250px]"/>
+
+              <img src="https://www.shutterstock.com/image-photo/cheerful-young-man-airline-worker-260nw-1815171710.jpg" alt="" className="hover:scale-110 duration-1000 w-[250px]"/>
+
+              <img src="https://www.shutterstock.com/image-photo/cheerful-young-man-airline-worker-260nw-1815171710.jpg" alt=""className="hover:scale-110 duration-1000 w-[250px]" />
+
+              <img src="https://www.shutterstock.com/image-photo/cheerful-young-man-airline-worker-260nw-1815171710.jpg" alt=""className="hover:scale-110 duration-1000 w-[250px]" />
+
+              <img src="https://www.shutterstock.com/image-photo/cheerful-young-man-airline-worker-260nw-1815171710.jpg" alt=""className="hover:scale-110 duration-1000 w-[250px]" />
+
+              <img src="https://www.shutterstock.com/image-photo/cheerful-young-man-airline-worker-260nw-1815171710.jpg" alt=""className="hover:scale-110 duration-1000 w-[250px]" />
+
+              <img src="https://www.shutterstock.com/image-photo/cheerful-young-man-airline-worker-260nw-1815171710.jpg" alt=""className="hover:scale-110 duration-1000 w-[250px]" />
+
+             </div>
+           </div> */}
+
+          {/* Message from director */}
+          <div className="w-full">
+            <h1 className="text-2xl font-semibold text-gray-700 p-2 text-center text-shadow">
+              Message From Director
+            </h1>
+            <div className="flex justify-center items-center gap-10 ">
+              <div data-aos="flip-up">
+                <img
+                  src={DirectorImg}
+                  alt="Director img"
+                  className=" w-[300px] h-[400px] border border-blue-900 box-shadow rounded-md"
+                />
+                <p className="text-center p-2 text-gray-800 font-bold text-xl hover:text-gray-600 ">Dr.Preeti Singh</p>
+              </div>
+              <div className="w-1/2 text-justify border box-shadow p-5 rounded-md m-2 bg-white leading-8" data-aos="fade-left">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Praesentium, dicta? Ea quae eligendi impedit deleniti
+                praesentium maxime reiciendis autem dolorem doloremque ex
+                molestiae quidem mollitia alias adipisci non nemo, eum commodi
+                iste molestias! Consequuntur repellendus sed tempora aperiam,
+                est pariatur quasi aut totam, rem temporibus asperiores ducimus
+                nesciunt veniam expedita! Ipsum dolores quaerat, enim nobis
+                ipsam cupiditate illo voluptatibus quia modi, qui magni officiis
+                facilis doloribus nemo beatae quam obcaecati quod saepe dolore
+                culpa provident deleniti deserunt laudantium. Accusamus
+                voluptatibus obcaecati expedita exercitationem adipisci dicta
+                sit deleniti eaque quis, officia eveniet ex id cum facere,
+                mollitia tenetur. Expedita pariatur numquam repudiandae
+                exercitationem voluptas dignissimos aut, voluptatum veniam sunt!
+                Possimus deleniti tenetur aperiam quo. Ut ducimus cumque
+                aspernatur totam officia quo omnis necessitatibus maiores
+                blanditiis dolor deleniti rem harum, quis qui etexercitationem
+                pariatur cum? Ad et, deleniti enim voluptates, itaque quidem
+                aspernatur eos, minima repellat veniam eius cupiditate!
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </HomeLayout>
   );
